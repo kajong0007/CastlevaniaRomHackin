9b7d2724f2ce01e09938d7d27a610d82cf3d5730
Jack O'Connor <joconnor@epochlabs.com>
2019-03-18 16:56:00 -0500

    trying to add the value from 6f to the screen
    might need to optimize cycles a bit by unrolling my subroutine calls

--- cv_nmi.asm
+++ cv_nmi.asm
@@ -3,14 +3,29 @@ NMI
   lda $18
   cmp #$05
   beq start
+
 hereisend
   lda $2002
   jmp interrupt_core+3
+
 drawsr
   sty $2006
   stx $2006
   sta $2007
   rts
+
+drawhexnum
+  cmp #$0a
+  bmi below9
+  sbc #$0a
+  ora #$e0
+  bmi number
+below9
+  ora #$d0
+number
+  jsr drawsr
+  rts
+
 start
   ldx #$7c
   ldy #$20
@@ -25,22 +40,31 @@ start
 
   lda $1a
   and #$0f
-  cmp #$0a
-  bmi below9
-  sbc #$0a
-  ora #$e0
-  bmi number
-below9
-  ora #$d0
-number
-  jsr drawsr
+  jsr drawhexnum
 
-;; Restore X and Y
-;  pla
-;  tay
-;  pla
-;  tax
-;  bvc hereisend
+  ldx #$9c
+  lda #$f1
+  jsr drawsr
+  inx
+
+  lda #$dd
+  jsr drawsr
+  inx
+
+  lda $6f
+; 209C-F
+  lsr
+  lsr
+  lsr
+  lsr
+  and #$0f
+  jsr drawhexnum
+
+  inx
+  lda $6f
+  and #$0f
+  jsr drawhexnum
+
   jmp hereisend
 
 interrupt_core = $c058
--- patch_it.sh
+++ patch_it.sh
@@ -8,70 +8,43 @@ THIS_DIR=$(dirname $0)
 IFILE="$THIS_DIR"/CV1.nes
 OFILE="$THIS_DIR"/modified_CV1.nes
 
-ASMFILE=("$THIS_DIR"/cv_nmi.asm "$THIS_DIR"/cv_busyloop.asm)
-OUTASM=("$THIS_DIR"/outnmi.a65 "$THIS_DIR"/outbusyloop.a65)
+ASMFILE="$THIS_DIR"/cv_nmi.asm
+OUTASM="$THIS_DIR"/outnmi.a65
 # size is +6 because of 1 extra byte, 1 extra jump, and 1 because i don't
 # remember the <= bash operator so i just added 1 and used less than
-MAXSIZE=(104 77)
-NUMASM=${#ASMFILE[*]}
-
-NMIOUT="${OUTASM[0]}"
-BUSYOUT="${OUTASM[1]}"
+MAXSIZE=200
 
 NMISIZE=0
-BUSYSIZE=0
 
 ASSEM="xa"
 
 i=0
 
-while [ $i -lt "$NUMASM" ]
-do
-  "$ASSEM" "${ASMFILE[$i]}" -o "${OUTASM[$i]}"
-  if [ "${MAXSIZE[$i]}" -lt $(stat -c"%s" "${OUTASM[$i]}") ]
-  then
-    echo "ERROR: File ${ASMFILE[$i]} exceeded max size ${MAXSIZE[$i]}"
-    exit 1
-  fi
-  ((++i))
-done
+"$ASSEM" "$ASMFILE" -o "$OUTASM"
+if [ "$MAXSIZE" -lt $(stat -c"%s" "$OUTASM") ]
+then
+  echo "ERROR: File $ASMFILE exceeded max size $MAXSIZE"
+  exit 1
+fi
 
-NMISIZE=$(stat -c'%s' "$NMIOUT")
+NMISIZE=$(stat -c'%s' "$OUTASM")
 NMIREALSIZE=$(( $NMISIZE - 3 ))
 
-BUSYSIZE=$(stat -c'%s' "$BUSYOUT")
-BUSYREALSIZE=$(( $BUSYSIZE - 3 ))
-
 #bf08
-#bf6c
 nmi_start_addr=$(echo '16i3F18p' | dc)
 nmi_end_addr=$(echo '16i1BF19p' | dc)
-busy_start_addr=$(echo '16i3F7Cp' | dc)
-busy_end_addr=$(echo '16i1BF7Dp' | dc)
 step=$(echo '16i4000p' | dc)
 
-
 #-1C068,4c,ad,bf
-#-1C04C,4c,eb,bf
 
 cp "$IFILE" "$OFILE"
 
 i=$nmi_start_addr
-j=$busy_start_addr
 while [ $i -lt $nmi_end_addr ]
 do
-  dd conv=notrunc if="$NMIOUT" bs=1 count=$NMIREALSIZE seek=$i of="$OFILE"
-  #dd conv=notrunc if="$BUSYOUT" bs=1 count=$BUSYREALSIZE seek=$j of="$OFILE"
+  dd conv=notrunc if="$OUTASM" bs=1 count=$NMIREALSIZE seek=$i of="$OFILE"
   i=$(( $i + $step ))
-  j=$(( $j + $step ))
 done
 
-dd conv=notrunc if="$NMIOUT" bs=1 count=3 skip=$NMIREALSIZE seek=$(echo '16i1C068p' | dc) of="$OFILE"
-#dd conv=notrunc if="$BUSYOUT" bs=1 count=3 skip=$BUSYREALSIZE seek=$(echo '16i1C04Cp' | dc) of="$OFILE"
+dd conv=notrunc if="$OUTASM" bs=1 count=3 skip=$NMIREALSIZE seek=$(echo '16i1C068p' | dc) of="$OFILE"
 
-#echo "$patch_list" | while read line
-#do
-#  byte_offset=$(echo "16i$(echo "$line" | cut -d',' -f1)p" | dc)
-#  bytes=$(echo ","$(echo "$line" | cut -d',' -f2-) | sed -e 's/,/\\x/g')
-#  echo -ne "$bytes" | dd conv=notrunc of="$OFILE" ibs=1 obs=1 seek=$byte_offset >/dev/null 2>&1
-#done
